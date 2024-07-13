2fee091c8c2738bf6883243209a427eaf91ce98f
Atharva Sawant <sawantatharva@protonmail.com>
2024-07-13 19:16:38 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import { Button } from 'react-bootstrap'
+
+function App() {
+  return (
+    <div className="App">
+    <h2>Hello World</h2>
+    <h3>Fuck bitches</h3>
+    <button>Normal Button</button>
+    <Button>Bootstrap button</Button>
+    </div>
+  );
+}
+
+export default App;
